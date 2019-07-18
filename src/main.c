f1cd67c8be3c6c7a32a174270086ec46e77f15a5
Christopher Nyberg <christopher.nyberg@gmail.com>
2019-07-17 18:33:25 -0700

    Learning variables

--- src/main.c
+++ src/main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// https://www.youtube.com/watch?v=KJgsSFOSQv0
 int main()
 {
-    printf("Hello world!\n");
+    // Let's make some variables
+    // String appears to be a list of char
+    char characterName[] = "John";
+    int characterAge = 35;
+
+    printf("There was once a man named %s.\n", characterName);
+    printf("He was %d years old.\n", characterAge);
+    printf("He really liked the name %s.\n", characterName);
+    printf("but did not like being %d.\n", characterAge);
+
     return 0;
 }
-
