d808792e1fc2f4ba5449e4b9af1c8ef534738b51
Christopher Nyberg <christopher.nyberg@gmail.com>
2019-07-17 18:59:13 -0700

    Learning about types

--- src/main.c
+++ src/main.c
@@ -1,13 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 // https://www.youtube.com/watch?v=KJgsSFOSQv0
 int main()
 {
-    // Let's make some variables
     // String appears to be a list of char
     char characterName[] = "John";
+
+    // Age of out character :: Int
     int characterAge = 35;
+
+    // Let's talk about Dtypes in C
+
+    // Integer
+    int integer = 40;
+
+    // Decimal Types
+
+    // Double
+    double gpa = 3.97;
+
+    // Float
+    float sortaPi = 3.14;
+
+    // Characters: You can store one single character
+    // Note characters are in single quotes.
+    char grade = 'A';
+
+    // String: basically an array of char
+    // Note double quotes here
+    char phrase[] = "This is a phrase";
+
+    // Math with two integers returns an integer.
+    // Math with a float coerces all to float
+    // Fun path functions
+    int twoCubed = pow(2, 3);
+
+    printf("twoCubed is %d.\n", twoCubed);
+
+    printf("This is a %s. This is an %c. This is a %f and this is a %d", characterName, grade, sortaPi, integer);
 
     printf("There was once a man named %s.\n", characterName);
     printf("He was %d years old.\n", characterAge);
