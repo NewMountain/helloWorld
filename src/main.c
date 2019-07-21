946f7e0854cff10ba953b7fb81736f96577a4dec
Christopher Nyberg <christopher.nyberg@gmail.com>
2019-07-21 15:29:56 -0700

    Structs introduced

--- src/main.c
+++ src/main.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <string.h>
 
 // https://www.youtube.com/watch?v=KJgsSFOSQv0
 // Stopped at: 56:00
+
+// Let's make a struct
+struct Student
+{
+    char name[50];
+    char major[50];
+    int age;
+    double gpa;
+};
+
 int main()
 {
     // String appears to be a list of char
@@ -33,6 +44,27 @@ int main()
     // Note double quotes here
     char phrase[] = "This is a phrase";
 
+    // CONSTANTS
+    const int NUM = 5;
+
+    // declare a variable for the user age
+    int age;
+
+    printf("Hey user: enter your age:\n");
+
+    scanf("%d", &age);
+
+    printf("You are %d years old.\n", age);
+
+    // Suppose you wanted to get a line
+    char someText[255];
+    printf("Write some text yo: ");
+    // Grabs a line of text
+    // Note, this isn't a pointer
+    fgets(someText, 255, stdin);
+
+    printf("You said:\n%s", someText);
+
     // Math with two integers returns an integer.
     // Math with a float coerces all to float
     // Fun path functions
@@ -52,5 +84,13 @@ int main()
     printf("He really liked the name %s.\n", characterName);
     printf("but did not like being %d.\n", characterAge);
 
+    // Declare the struct
+    struct Student student1;
+    student1.age = 22;
+    student1.gpa = 3.2;
+    //
+    strcpy(student1.name, "Jim");
+    strcpy(student1.name, "Business");
+
     return 0;
-}
+};
